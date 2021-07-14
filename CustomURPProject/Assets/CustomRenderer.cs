20d1aee079f8866d3612f54bd2919774b689def9
zhangwei_modooplay <wei.zhang@modooplay.com>
2021-07-14 11:18:53 +0800

    Init

---
+++ CustomURPProject/Assets/CustomRenderer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class CustomRenderer : ScriptableRenderer
+{
+    public CustomRenderer(ScriptableRendererData data) : base(data)
+    {
+        
+    }
+
+    public override void Setup(ScriptableRenderContext context, ref RenderingData renderingData)
+    {
+    }
+}
