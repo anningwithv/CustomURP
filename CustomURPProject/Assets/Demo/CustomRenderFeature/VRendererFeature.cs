81aa7fcf71f452ddebbf13cf690ca133b1f57609
zhangwei_modooplay <wei.zhang@modooplay.com>
2021-07-15 18:07:44 +0800

    CustomRendererFeature

---
+++ CustomURPProject/Assets/Demo/CustomRenderFeature/VRendererFeature.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class VRendererFeature : ScriptableRendererFeature
+{
+    public Settings settings = new Settings();
+
+    private VRendererPass m_VRenderPass = null;
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        renderer.EnqueuePass(m_VRenderPass);
+    }
+
+    public override void Create()
+    {
+        m_VRenderPass = new VRendererPass(settings, "VRenderPass", settings.Event);
+    }
+
+    [System.Serializable]
+    public class Settings
+    {
+        public Mesh mesh;
+        public Material material;
+        public RenderPassEvent Event = RenderPassEvent.AfterRenderingOpaques;
+    }
+}
---
+++ CustomURPProject/Assets/Demo/CustomRenderFeature/VRendererPass.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class VRendererPass : ScriptableRenderPass
+{
+    private ProfilingSampler m_ProfilingSampler;
+
+    private FilteringSettings m_FilteringSettings;
+    private string m_ProfilerTag;
+    private VRendererFeature.Settings m_Settings;
+
+    public VRendererPass(VRendererFeature.Settings settings, string profilerTag, RenderPassEvent renderPassEvent)
+    {
+        m_Settings = settings;
+        m_ProfilerTag = profilerTag;
+        m_ProfilingSampler = new ProfilingSampler(profilerTag);
+
+        this.renderPassEvent = renderPassEvent;
+    }
+
+    public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+    {
+        CommandBuffer cmd = CommandBufferPool.Get();
+        using (new ProfilingScope(cmd, m_ProfilingSampler))
+        {
+            cmd.DrawMesh(m_Settings.mesh, Matrix4x4.identity, m_Settings.material);
+        }
+
+        context.ExecuteCommandBuffer(cmd);
+        cmd.Clear();
+        CommandBufferPool.Release(cmd);
+    }
+}
